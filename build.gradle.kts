8fdaa1f5066a62301dca1e6633d542579d8803fd
Gabriel <114709637+gblrodrigues@users.noreply.github.com>
2026-07-07 08:02:05 -0300

    chore: prepare application for deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
